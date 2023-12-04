994a926279621b8bf8bbf3cea0b6d075c0b4c960
Flaviant3 <flaviant3@gmail.com>
2023-12-04 18:26:16 +0100

    deuxiemem commit avec cam en plus

--- affiche.php
+++ affiche.php
@@ -1,17 +1,11 @@
 <?php
 ini_set('display_errors', 1);
-/* *
-*  connexion bdd
-* */
-// connexion en local 
 $db = new PDO('mysql:host=localhost;dbname=chat', 'root', 'root');
 
-// requête pour obtenir les 10 derniers messages
-$statement = $db->query('SELECT * FROM chat ORDER BY chat_id DESC LIMIT 10');
-
+$statement = $db->query('SELECT * FROM chat ORDER BY chat_id ASC LIMIT 30');
 $messages = $statement->fetchAll();
 
-// affichage des messages
 foreach ($messages as $message) {
-    echo '<strong>' . $message['chat_pseudo'] . '</strong> > ' . $message['chat_message'] . ' <br />';
+    echo '<strong>' . $message['chat_pseudo'] . '</strong> > ' . $message['chat_message'] . ' <span style="color: #888; font-size: 0.8em;">(' . $message['chat_date'] . ')</span><br />';
 }
+?>
--- dire.php
+++ dire.php
@@ -1,20 +1,15 @@
 <?php
-// dire.php
 try {
-    // connexion à la base de données
     $db = new PDO('mysql:host=localhost;dbname=chat', 'root', 'root');
     
-    // récupération des variables GET
-    $phrase = $_GET['message'];
-    $pseudo = $_GET['pseudo'];
+    $phrase = $_GET['chat_message'];
+    $pseudo = $_GET['chat_pseudo'];
     
-    // requête préparée pour éviter les injections SQL
-    $statement = $db->prepare('INSERT INTO chat (chat_pseudo, chat_message, date) VALUES (?, ?, NOW())');
+    $statement = $db->prepare('INSERT INTO chat (chat_pseudo, chat_message, chat_date) VALUES (?, ?, NOW())');
     $statement->execute([$pseudo, $phrase]);
 
-    // envoi d'une réponse pour le débogage
     echo 'Message enregistré avec succès!';
 } catch (PDOException $e) {
-    // gestion des erreurs de connexion ou d'exécution de la requête
     echo 'Erreur : ' . $e->getMessage();
 }
+?>
